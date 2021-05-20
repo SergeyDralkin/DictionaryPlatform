a9d667a279b4f7d6545fd6febc39c38f189680e8
WarPackSega67 <Scizor67@yandex.ru>
2021-05-20 16:59:43 +0300

    FixNewFunction#2

--- RusDictionary/MainForm.Designer.cs
+++ RusDictionary/MainForm.Designer.cs
@@ -1753,6 +1753,7 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.MinimumSize = new System.Drawing.Size(1084, 691);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
